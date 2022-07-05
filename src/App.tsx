dd8ddf46411d70b5202a4b13e89473e299d89f41
Vineeth K <vineeth.freelanzr@gmail.com>
2022-07-05 15:34:03 +0530

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
